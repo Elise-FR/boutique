a47aff5391498e7cd30d0f7844e68fe585ee9447
Elise-FR <elise.franzini@EDG>
2021-05-20 16:54:45 +0200

    etape 1 modifiée - affichage du tableau avec html

--- simple-catalog.php
+++ simple-catalog.php
@@ -1,18 +1,30 @@
 <html>
     <head></head>
     <body>
+
+
+
 <?php 
 $products = ["roue", "planche","stickers"];
+
+?>
+
+
+<?php
 $first_product = reset($products);
 $last_product = end($products);
 sort($products);
-print_r($products);
 echo "<br/>";
 echo "My first product is $first_product <br/>";
 echo "My last product is $last_product";
 
-
-
 ?>
+
+<ls>
+    <li><?php echo $products[0]?></li> 
+    <li><?php echo $products[1]?></li>
+    <li><?php echo $products[2]?></li>
+</ls>
+
 </body>
 </html>
