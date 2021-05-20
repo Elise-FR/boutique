a49fa6077b43787f787815c5d7a53d2b02f2942b
Elise-FR <elise.franzini@EDG>
2021-05-20 14:05:42 +0200

    affichage du premier produit et du dernier produit

--- simple-catalog.php
+++ simple-catalog.php
@@ -1,9 +1,18 @@
-
+<html>
+    <head></head>
+    <body>
 <?php 
 $products = ["roue", "planche","stickers"];
-
+$first_product = reset($products);
+$last_product = end($products);
 sort($products);
 print_r($products);
+echo "<br/>";
+echo "My first product is $first_product <br/>";
+echo "My last product is $last_product";
+
 
 
 ?>
+</body>
+</html>
