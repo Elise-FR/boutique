a5df59e31818ef72a3a163688f850678794dce77
Elise-FR <elise.franzini@EDG>
2021-05-20 11:03:26 +0200

    Exercices affichage prix, name, url et footer header

---
+++ item.php
@@ -0,0 +1,26 @@
+
+<html lang="fr">
+<head> </head>
+<body>
+    
+
+
+<?php 
+
+include("header.php");
+
+
+$name = "Longboard";
+$prix = "179€";
+$url = "https://cdn.shopify.com/s/files/1/0040/7807/6033/products/I-Grande-95593-longboard-globe-blazer-xl-coconut-lime-36.net_250x250@2x.jpg?v=1612965566";
+
+
+echo "$name <br/> $prix <br/> <img src='$url'/>";
+
+include("footer.php");
+
+
+?>
+
+</body>
+</html>
