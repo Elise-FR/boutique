d1f8bb907b88c0c0aa76336ea696f3002b02574e
Elise F <elise.franzini-ruynat@le-campus-numerique.fr>
2021-05-21 09:25:52 +0200

    ajout des fichier include

---
+++ team.php
@@ -0,0 +1,145 @@
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+ 
+    <!--Début HTML Team-->
+
+    <div class="container marketing">
+
+      <div class="row">
+        <div class="col-12 col-lg-6">
+
+          <div class="row">
+              <a href="https://fr.linkedin.com/">
+            <img 
+            src="./photo/frame-robin.jpg" 
+            class="img-fluid" 
+            alt="photo Robin">
+        </a>
+        </div>
+
+<div class="col text-center">
+          <h2>Robin Poulet</h2>
+          <div class="col"><span class="metier">Chef de Projet</span></div>
+          <div class="col"><span class="description">"Amoureux du Whisky"</span></div>
+    
+          </div>
+            <div class="p-3 mb-2 bg-dark text-white">
+            <p class="texte-pres">
+            Actuellement en reconversion après une carrière dans la comptabilité <br/>
+            C'est avec motivation que je me suis lancé dans une formation de développeur web <br/>
+            Je reste à votre disposition pour tout renseignement complémentaire. <br/>
+          </p>
+         </div>
+          <p><a class="btn btn-secondary" href="cv_bootstrap_robin.html">En savoir plus &raquo;</a></p>
+        </div>
+       
+        <div class="col-12 col-lg-6">
+
+        <div class="row">
+            <a href="https://www.linkedin.com/in/elise-franzini-ruynat-723037140/">
+           <img 
+            src="./photo/frame-elise.jpg" 
+            class="img-fluid" 
+            alt="photo elise"> 
+        </a>
+     
+        </div>
+
+        <div class="col text-center">
+          <h2>Elise Franzini</h2>
+          <div class="col"><span class="metier">Community Manager</span></div>
+          <div class="col"><span class="description">"Passionnée de sport"</span></div>
+        </div>
+
+          <div class="p-3 mb-2 bg-dark text-white">
+          <p class="texte-pres">
+          Actuellement en formation au Campus in the Alps de Grenoble <br/>
+          J'ai pour projet de me lancer dans de nouveaux challenges et intégrer le domaine du développement Web. <br/>
+          Rigoureuse et méthodique je saurai mettre mon expérience à profit afin de pouvoir être la plus efficace possible dans mon travail.<br/>
+          </p>
+        </div>
+          <p><a class="btn btn-secondary" href="cv_bootstrap_elise.html">En savoir plus &raquo;</a></p>
+        </div>
+    </div>
+
+
+        <!--2eme ligne-->
+    <div class="row">
+        <div class="col-12 col-lg-6">
+          <div class="row">
+            <a href="https://www.linkedin.com/in/lucas-bokser-1b5866a8/">
+            <img 
+            src="./photo/frame-lucas.jpg" 
+            class="img-fluid" 
+            alt="photo Lucas">
+     </a>
+        </div>
+
+    <div class="col text-center">
+          <h2>Lucas Bokser</h2>
+         <div class="col"><span class="metier">Développeur Web</span></div>
+          <div class="col"><span class="description">"Viking au régime"</span></div>
+
+        </div>
+          <div class="p-3 mb-2 bg-dark text-white">
+        
+
+          <p class="texte-pres">
+            Dynamique, en soif d'apprentissage, curieux par nature, passionné d'informatique <br/>
+            et maîtrisant parfaitement la langue anglaise ;<br/>
+            aujourd’hui, je souhaite mettre mes compétences et ma motivation à
+            votre service. <br />
+            N'hésitez pas à prendre contact avec moi pour toutes demandes de renseignements. <br />
+          </p>
+          </div>
+          <p><a class="btn btn-secondary" href="cv_bootstrap_lucas.html">En savoir plus &raquo;</a></p>
+        </div>
+    
+            <div class="col-12 col-lg-6">
+
+              <div class="row">
+
+                <a href="https://www.instagram.com/smith2k__/">
+                
+                <img  src="./photo/frame-chris.jpg" 
+                class="img-fluid " 
+                alt="photo Chris">
+            </a>
+             
+            </div>
+
+            <div class="col text-center">
+              <h2>Christophe Haller</h2>
+              <div class="col"><span class="metier">Designer</span></div>
+              <div class="col"><span class="description">"A trop envie de fumer"</span></div>
+            </div>
+              <div class="p-3 mb-2 bg-dark text-white">
+                
+
+              <p class="texte-pres">
+                Actuellement en formation au Campus in the Alp’s de Grenoble<br />  
+                Je souhaite me lancer dans de nouveaux défis et intégrer le domaine du développement informatique. <br />
+                Je suis passionné et prêt à vous apporter mon expérience pour faire grandir vos projets<br/>
+              </p>
+            </div>
+              <p><a class="btn btn-secondary" href="cv_bootstrap_christophe.html">En savoir plus &raquo;</a></p>
+            </div>
+        </div>
+      </div>
+      
+   
+
+    <!--Fin HTML Team-->
+
+    
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
