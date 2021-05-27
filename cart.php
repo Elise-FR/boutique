02d2814acedfe16f01826654a2ba9ed0b38fb570
Elise F <elise.franzini-ruynat@le-campus-numerique.fr>
2021-05-27 16:40:47 +0200

    Commencement étape 2

---
+++ cart.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "la quantité demandée est de : " . $_POST["choice"] . "<br>";
+echo "le nom du produit : " . $_POST["name"] . "<br>";
+echo "le Prix du produit : " . $_POST["price"];
+
+//var_dump($_POST);
+
+
+?>
