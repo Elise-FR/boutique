cc56f144aaffa41b8c7f1fd526b0e904c084d16d
Elise-FR <elise.franzini@Digital-Grenoble.local>
2021-05-28 14:40:23 +0200

    obligé de l'ajouter

---
+++ php-ex/item.php
@@ -0,0 +1,15 @@
+<?php 
+
+$name = "Longboard";
+$prix = "179€";
+$url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRb-3ct_hJMI9WdCjYKtf72x1qk5V0PeatUA&usqp=CAU";
+
+echo "<img src = '$url'/>";
+
+echo $name;
+echo $prix;
+
+
+
+
+?>
