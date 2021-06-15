792970a9133f82c1b83c84bc5f4aff4320455ca2
Elise-FR <elise.franzini@EDG>
2021-06-15 13:05:24 +0200

    itération 3 : création class panier + fonction addmypanier

--- Panier.php
+++ Panier.php
@@ -1,9 +1,48 @@
 <?php
 
-class Panier 
+class Panier
 
 {
 
-public $myArray =[];
+    public $myPanier = [];
+    public $quantity;
+
+    //méthode qui ajoute un article ou ajoute une quantité à un article déja présent
+
+    public function addAtMyPanier($article_id,$quantity)
+
+    {
+        if (isset($this->myPanier[$article_id])) {
+
+
+            $this->myPanier[$article_id] += $quantity; 
+        }
+
+        else {
+
+          $this->myPanier[$article_id] = $quantity;
+
+        }
+        
+    }
+
+    //méthode qui ajoute quantité à la ligne de panier correspondant à l'article
+
+    public function updateMyPanier($article_id,$quantity)
+
+    {
+
+
+
+
+    }
+
+    //fonction pour afficher mon panier
+
+    public function deleteMyPanier($article_id)
+
+    {
+
+    }
 
 }
